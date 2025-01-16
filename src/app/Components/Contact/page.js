dc1c0da040fb70eb3265a0041f7f1f34c349f453
Mickaelio <94882142+tsiheje@users.noreply.github.com>
2025-01-16 16:07:18 +0300

    refact : page contact

--- src/app/Components/Contact/page.js
+++ src/app/Components/Contact/page.js
@@ -1,3 +1,4 @@
+'use client';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
