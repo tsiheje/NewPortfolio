7fc6aea10ed7909012fbe45579d07c8d103050a2
Mickaelio <94882142+tsiheje@users.noreply.github.com>
2025-01-18 18:06:28 +0300

    refact : analytics

--- src/app/Components/Projects/page.js
+++ src/app/Components/Projects/page.js
@@ -1,87 +1,7 @@
 "use client";
-import Image from 'next/image';
-import HTML from "../../Assets/Images/Html.png";
-import Ai from "../../Assets/Images/Ai.png";
-import MySQL from "../../Assets/Images/Mysql.png";
-import Native from "../../Assets/Images/Native.png";
 import { FaFolderOpen } from "react-icons/fa";
+import Image from 'next/image';
 
-const projects = [
-    {
-        src: HTML,
-        alt: "Projet 1",
-        description: "A web application built with HTML and CSS, focusing on responsive design.",
-        technologies: ["HTML", "CSS", "JavaScript"],
-    },
-    {
-        src: Ai,
-        alt: "Projet 2",
-        description: "A machine learning project using AI technologies for data analysis.",
-        technologies: ["Python", "TensorFlow", "AI/ML"],
-    },
-    {
-        src: MySQL,
-        alt: "Projet 3",
-        description: "A full-stack application with MySQL for efficient database management.",
-        technologies: ["Node.js", "Express", "MySQL"],
-    },
-    {
-        src: Native,
-        alt: "Projet 4",
-        description: "A mobile app developed using React Native for cross-platform compatibility.",
-        technologies: ["React Native", "Expo", "Firebase"],
-    },
-];
-
-const ProjectCard = ({ project, index }) => (
-    <div className="flex flex-col lg:flex-row bg-white rounded-2xl shadow-xl p-6 w-full max-w-4xl mb-8 transition-all duration-300 hover:scale-105">
-        {index % 2 === 0 ? (
-            <>
-                <div className="flex-1">
-                    <Image
-                        src={project.src}
-                        alt={project.alt}
-                        className="rounded-lg w-full h-[250px] sm:h-[300px] md:h-[350px] lg:h-[400px] object-cover mx-auto"
-                    />
-                </div>
-                <div className="flex-1 pl-6 mt-4 lg:mt-0">
-                    <h3 className="text-2xl font-semibold text-gray-800">{project.alt}</h3>
-                    <p className="text-gray-600 mt-2">{project.description}</p>
-                    <div className="mt-4">
-                        <strong className="text-gray-800">Technologies:</strong>
-                        <ul className="list-disc pl-5 text-gray-600">
-                            {project.technologies.map((tech, index) => (
-                                <li key={index}>{tech}</li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-            </>
-        ) : (
-            <>
-                <div className="flex-1 pr-6 mt-4 lg:mt-0">
-                    <h3 className="text-2xl font-semibold text-gray-800">{project.alt}</h3>
-                    <p className="text-gray-600 mt-2">{project.description}</p>
-                    <div className="mt-4">
-                        <strong className="text-gray-800">Technologies:</strong>
-                        <ul className="list-disc pl-5 text-gray-600">
-                            {project.technologies.map((tech, index) => (
-                                <li key={index}>{tech}</li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-                <div className="flex-1">
-                    <Image
-                        src={project.src}
-                        alt={project.alt}
-                        className="rounded-lg w-full h-[250px] sm:h-[300px] md:h-[350px] lg:h-[400px] object-cover mx-auto"
-                    />
-                </div>
-            </>
-        )}
-    </div>
-);
 
 const Projects = () => {
     return (
@@ -95,9 +15,7 @@ const Projects = () => {
                     🌟 Dive into my innovative projects, where I leverage my expertise in front-end development to design and implement distinctive, high-performing web solutions that not only meet but exceed user expectations, combining creativity, technical precision, and a user-centered approach to deliver exceptional digital experiences.
                 </p>
                 <div className="flex flex-col items-center justify-center gap-8">
-                    {projects.map((project, index) => (
-                        <ProjectCard key={index} project={project} index={index} />
-                    ))}
+                    
                 </div>
             </div>
         </section>
